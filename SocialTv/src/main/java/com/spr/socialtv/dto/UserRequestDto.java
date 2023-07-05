f99437e03010eb3c854053dee95d4f26bd4c1717
sxi8022 <sxi8022@gmail.com>
2023-07-05 11:59:21 +0900

    로그아웃 기능 구현

--- SocialTv/src/main/java/com/spr/socialtv/dto/UserRequestDto.java
+++ SocialTv/src/main/java/com/spr/socialtv/dto/UserRequestDto.java
@@ -1,5 +1,6 @@
 package com.spr.socialtv.dto;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,5 +10,26 @@ public class UserRequestDto {
     private String username;
     private String password;
     private String selfText;
+
+
+    @Getter
+    @Setter
+    public static class Reissue {
+        @NotEmpty(message = "accessToken 을 입력해주세요.")
+        private String accessToken;
+
+        @NotEmpty(message = "refreshToken 을 입력해주세요.")
+        private String refreshToken;
+    }
+
+    @Getter
+    @Setter
+    public static class Logout {
+        @NotEmpty(message = "잘못된 요청입니다.")
+        private String accessToken;
+
+        @NotEmpty(message = "잘못된 요청입니다.")
+        private String refreshToken;
+    }
 
 }
--- SocialTv/src/main/java/com/spr/socialtv/security/UserDetailsServiceImpl.java
+++ SocialTv/src/main/java/com/spr/socialtv/security/UserDetailsServiceImpl.java
@@ -1,18 +1,31 @@
 package com.spr.socialtv.security;
 
+import com.spr.socialtv.dto.HttpResponseDto;
+import com.spr.socialtv.dto.UserRequestDto;
 import com.spr.socialtv.entity.User;
+import com.spr.socialtv.jwt.JwtUtil;
 import com.spr.socialtv.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final UserRepository userRepository;
+
+    private final JwtUtil jwtUtil;
+    private final RedisTemplate redisTemplate;
+    private final HttpResponseDto responseDto;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -21,4 +34,30 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
         return new UserDetailsImpl(user);
     }
+
+
+    public ResponseEntity<?> logout(UserRequestDto.Logout logout) {
+        // 1. Access Token 검증
+        if (!jwtUtil.validateToken(logout.getAccessToken())) {
+            return responseDto.fail("잘못된 요청입니다.", HttpStatus.BAD_REQUEST);
+        }
+
+        // 2. Access Token 에서 User email 을 가져옵니다.
+        Authentication authentication = jwtUtil.getAuthentication(logout.getAccessToken());
+
+        // 3. Redis 에서 해당 User email 로 저장된 Refresh Token 이 있는지 여부를 확인 후 있을 경우 삭제합니다.
+        if (redisTemplate.opsForValue().get("RT:" + authentication.getName()) != null) {
+            // Refresh Token 삭제
+            redisTemplate.delete("RT:" + authentication.getName());
+        }
+
+        // 4. 해당 Access Token 유효시간 가지고 와서 BlackList 로 저장하기
+        Long expiration = jwtUtil.getExpiration(logout.getAccessToken());
+        redisTemplate.opsForValue()
+                .set(logout.getAccessToken(), "logout", expiration, TimeUnit.MILLISECONDS);
+
+        return responseDto.success("로그아웃 되었습니다.");
+    }
+
+
 }
